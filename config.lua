3a144536b48486c50eadaf7f4eb09d59561c9467
kmafeni04 <komemafeni944@gmail.com>
2024-07-02 11:46:19 +0100

    Railway test 1

--- config.lua
+++ config.lua
@@ -2,20 +2,20 @@ local config = require("lapis.config")
 
 config({ "development", "production" }, {
   server = "nginx",
-  sqlite = {
-    database = "app.sqlite",
-  }
 })
 
 config("development", {
   code_cache = "off",
   num_workers = "4",
+  sqlite = {
+    database = "app.sqlite",
+  }
 })
 
 config("production", {
   code_cache = "on",
   num_workers = "1",
-  -- sqlite = {
-  --   database = "/var/data/app.sqlite",
-  -- }
+  sqlite = {
+    database = "/data/app.sqlite",
+  }
 })
